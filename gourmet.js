1e7c719adac76d5fdc04fe9b18628f00b2649ef2
G485192024 <248519@st.takushoku-u.ac.jp>
2025-06-04 23:28:13 +0900

    検索機能完成バージョン

--- gourmet.js
+++ gourmet.js
@@ -16,19 +16,26 @@ function print(data) {
   }
 }
 
-//課題４−２
-function Shop() {
-
-  let input = document.querySelector('#keyword');
-  let keyword = input.value;
-
-  console.log("検索キー: " + keyword);
-  
-}
-
 // 課題5-1 の関数 printDom() はここに記述すること
 function printDom(data) {
   let resultDiv = document.querySelector('#result');
+
+  let h2s = document.querySelectorAll('h2');
+  for (let h2 of h2s) {
+    h2.remove();
+  }
+  let h3s = document.querySelectorAll('h3');
+  for (let h3 of h3s) {
+    h3.remove();
+  }
+  let h4s = document.querySelectorAll('h4');
+  for (let h4 of h4s) {
+    h4.remove();
+  }
+  let uls = document.querySelectorAll('ul');
+  for (let ul of uls) {
+    ul.remove();
+  }
 
   let shops = data.results.shop;
 
@@ -39,13 +46,11 @@ function printDom(data) {
   for (let i = 0; i < shops.length; i++) {
     let shop = shops[i];
 
-    let p = document.createElement('p');
-    p.textContent = "検索結果" + (i + 1) + "件目";
-    resultDiv.insertAdjacentElement('beforeend',p);
+    let count = document.createElement('h3');
+    count.textContent = "検索結果" + (i + 1) + "件目";
+    resultDiv.insertAdjacentElement('beforeend',count);
 
-    let ul = document.createElement('ul');
-
-    let jyoho = ["名前", "アクセス", "住所", "予算", "キャッチコピー","ジャンル", "営業時間", "最寄駅", "サブジャンル"];   
+    let jyoho = ["名前", "アクセス", "住所", "予算", "キャッチコピー","ジャンル", "営業時間", "最寄駅"];   
     let values = [
       shop.name,
       shop.access,
@@ -55,12 +60,19 @@ function printDom(data) {
       shop.genre.name,
       shop.open,
       shop.station_name,
-      shop.sub_genre.name
       ];
-    for (let i = 0;i < jyoho.length; i++) {
+
+    let ul = document.createElement('ul');
+
+    for (let j = 0;j < jyoho.length; j++) {
       let li = document.createElement('li');
-      li.textContent = jyoho[i] + ": " + values[i];
+      li.textContent = jyoho[j] + ": " + values[j];
       ul.insertAdjacentElement('beforeend',li);
+    }
+    if (shop.sub_genre != undefined && shop.sub_genre.name != undefined ) {
+      let li = document.createElement('li');
+      ul.insertAdjacentElement('beforeend', li);
+      li.textContent = "サブジャンル: " + shop.sub_genre.name;
     }
     resultDiv.insertAdjacentElement('beforeend',ul);
   }
@@ -68,17 +80,90 @@ function printDom(data) {
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
 
-
-
+let b = document.querySelector('button#kensaku');
+    b.addEventListener('click',sendRequest)
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
+  let input = document.querySelector('input');
+  let genre = input.value
+  let resultDiv = document.querySelector('#result');
 
+  input.value = "";
+
+  let h2s = document.querySelectorAll('h2');
+  for (let h2 of h2s) {
+    h2.remove();
+  }
+  let h3s = document.querySelectorAll('h3');
+  for (let h3 of h3s) {
+    h3.remove();
+  }
+  let h4s = document.querySelectorAll('h4');
+  for (let h4 of h4s) {
+    h4.remove();
+  }
+  let uls = document.querySelectorAll('ul');
+  for (let ul of uls) {
+    ul.remove();
+  }
+  if (genre === "") {
+    let h4s = document.createElement('h4');
+    h4s.textContent = "探したい語句を教えてね ヽ(・∀・)ノ";
+    resultDiv.insertAdjacentElement('beforeend', h4s);
+    return;
+  } else if (genre == "居酒屋") {
+    genre = "G001";
+  } else if (genre == "ダイニングバー・バル") {
+    genre = "G002";
+  } else if (genre == "創作料理") {
+    genre = "G003";
+  } else if (genre == "和食") {
+    genre = "G004";
+  } else if (genre == "洋食") {
+    genre = "G005";
+  } else if (genre == "イタリアン・フレンチ") {
+    genre = "G006";
+  } else if (genre == "中華") {
+    genre = "G007";
+  } else if (genre == "焼肉・ホルモン") {
+    genre = "G008";
+  } else if (genre == "アジア・エスニック料理") {
+    genre = "G009";
+  } else if (genre == "各国料理") {
+    genre = "G010";
+  } else if (genre == "カラオケ・パーティ") {
+    genre = "G011";
+  } else if (genre == "バー・カクテル") {
+    genre = "G012";
+  } else if (genre == "ラーメン") {
+    genre = "G013";
+  } else if (genre == "カフェ・スイーツ") {
+    genre = "G014";
+  } else if (genre == "その他グルメ") {
+    genre = "G015";
+  } else if (genre == "お好み焼き・もんじゃ") {
+    genre = "G016";
+  } else if (genre == "韓国料理") {
+    genre = "G017";
+  }
+
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/hotpepper/' + genre + '.json';
+
+  axios.get(url)
+    .then(showResult)  
+    .catch(showError)  
+    .then(finish);    
 }
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
+  let data = resp.data;
 
+  if (typeof data === 'string') {
+    data = JSON.parse(data);
+  } 
+    printDom(data);
 }
 
 // 課題6-1: 通信エラーが発生した時の処理
@@ -90,209 +175,43 @@ function showError(err) {
 function finish() {
     console.log('Ajax 通信が終わりました');
 }
+// おすすめのキーワード
+function suggest() {
 
-////////////////////////////////////////
-// 以下はグルメのデータサンプル
-// 注意: 第5回までは以下を変更しないこと！
-// 注意2: 課題6-1 で以下をすべて削除すること
-let data = {
-  "results": {
-    "api_version": "1.26",
-    "results_available": 52,
-    "results_returned": "10",
-    "results_start": 1,
-    "shop": [
-      {
-        "access": "京王八王子駅を背にし右手に見えるローソンの隣のビル、ホテルザ・ビーの2階です。",
-        "address": "東京都八王子市明神町４-6-12　ホテル・ザ・ビー八王子（旧八王子プラザホテル）2F",
-        "band": "可",
-        "barrier_free": "なし",
-        "budget": {
-          "average": "ディナー3000円",
-          "code": "B003",
-          "name": "3001～4000円"
-        },
-        "budget_memo": "",
-        "capacity": 100,
-        "card": "利用可",
-        "catch": "【サプライズ演出有】 結婚パーティー受付中",
-        "charter": "貸切可 ：VIPルーム1部屋2～15名様前後（完全個室）/パーティーフロア！50型モニター・スポットライト等充実",
-        "child": "お子様連れOK",
-        "close": "不定休有り。店休時は店長携帯電話09035324825まで問い合わせください♪",
-        "coupon_urls": {
-          "pc": "https://www.hotpepper.jp/strJ000989843/map/?vos=nhppalsa000016",
-          "sp": "https://www.hotpepper.jp/strJ000989843/scoupon/?vos=nhppalsa000016"
-        },
-        "course": "あり",
-        "english": "なし",
-        "free_drink": "あり",
-        "free_food": "なし",
-        "genre": {
-          "catch": "大人の社交場非日常を味わうダイニング",
-          "code": "G001",
-          "name": "居酒屋"
-        },
-        "horigotatsu": "なし",
-        "id": "J000989843",
-        "karaoke": "あり",
-        "ktai_coupon": 0,
-        "large_area": {
-          "code": "Z011",
-          "name": "東京"
-        },
-        "large_service_area": {
-          "code": "SS10",
-          "name": "関東"
-        },
-        "lat": 35.6585460152,
-        "lng": 139.34327231,
-        "logo_image": "https://imgfp.hotp.jp/IMGH/21/04/P038512104/P038512104_69.jpg",
-        "lunch": "なし",
-        "middle_area": {
-          "code": "Y110",
-          "name": "八王子・立川"
-        },
-        "midnight": "営業していない",
-        "mobile_access": "京王八王子駅1分/JR八王子駅北口5分",
-        "name": "バグダッドカフェ Bagdadcafe/モータウン MOTOWN",
-        "name_kana": "ばぐだっどかふぇもーたうんはちおうじ",
-        "non_smoking": "禁煙席なし",
-        "open": "月～日、祝日、祝前日: 17:00～21:00 （料理L.O. 20:00 ドリンクL.O. 20:00）",
-        "other_memo": "ステージ・マイク・カラオケ等",
-        "parking": "なし",
-        "party_capacity": 250,
-        "pet": "不可",
-        "photo": {
-          "mobile": {
-            "l": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_168.jpg",
-            "s": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_100.jpg"
-          },
-          "pc": {
-            "l": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_238.jpg",
-            "m": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_168.jpg",
-            "s": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_58_s.jpg"
-          }
-        },
-        "private_room": "あり ：VIPルーム1部屋2～15名様前後（完全個室）/パーティーフロア",
-        "service_area": {
-          "code": "SA11",
-          "name": "東京"
-        },
-        "shop_detail_memo": "ご質問、ご要望はお気軽にお問い合わせください♪",
-        "show": "あり",
-        "small_area": {
-          "code": "X220",
-          "name": "八王子"
-        },
-        "station_name": "京王八王子",
-        "sub_genre": {
-          "code": "G002",
-          "name": "ダイニングバー・バル"
-        },
-        "tatami": "なし",
-        "tv": "あり",
-        "urls": {
-          "pc": "https://www.hotpepper.jp/strJ000989843/?vos=nhppalsa000016"
-        },
-        "wedding": "大歓迎！ステージ・マイク・音響・映像等、設備充実！！最大200名様まで是非どうぞ★",
-        "wifi": "あり"
-      },
-      {
-        "access": "ＪＲ八王子駅北口徒歩1分/京王八王子駅徒歩3分",
-        "address": "東京都八王子市東町12-14",
-        "band": "不可",
-        "barrier_free": "あり ：スタッフがお手伝いさせて頂きます。ご不明な点等お気軽にお声掛け下さい。事前のお問い合わせも◎",
-        "budget": {
-          "average": "2,000円(通常平均) 3,000円(宴会平均) ",
-          "code": "B002",
-          "name": "2001～3000円"
-        },
-        "budget_memo": "",
-        "capacity": 100,
-        "card": "利用可",
-        "catch": "2.5時間飲み放題付2000円~ 【★少人数様ソファー★】",
-        "charter": "貸切可 ：八王子で人気の個室バル最大50名様まで対応できます。20名様～フロア貸切応相談　最大100名様までOK",
-        "child": "お子様連れ歓迎 ：チャイルドシートもご用意しています。お子様連れのママ会・ご家族でのご利用にもお気軽にどうぞ♪",
-        "close": "【年中無休】貸切宴会のご予約も絶賛受付中！最大100名様までご利用可能ですので、会社宴会 同窓会 宴会にオススメ♪",
-        "coupon_urls": {
-          "pc": "https://www.hotpepper.jp/strJ001275177/map/?vos=nhppalsa000016",
-          "sp": "https://www.hotpepper.jp/strJ001275177/scoupon/?vos=nhppalsa000016"
-        },
-        "course": "あり",
-        "english": "なし",
-        "free_drink": "あり ：★NEW OPEN★当店イチ押しのコース★2.5h飲み放題付き「Funny Classicコース」4980円→2980円",
-        "free_food": "あり ：★NEW OPEN★当店イチ押しのコース★2.5h飲み放題付き「Funny Classicコース」4980円→2980円",
-        "genre": {
-          "catch": "八王子 個室居酒屋 飲み放題 肉バル 女子会",
-          "code": "G001",
-          "name": "居酒屋"
-        },
-        "horigotatsu": "なし ：温かな照明と開放的な店内で楽しくご宴会♪八王子での宴会 飲み会 女子会 二次会 合コンに◎",
-        "id": "J001275177",
-        "karaoke": "なし",
-        "ktai_coupon": 0,
-        "large_area": {
-          "code": "Z011",
-          "name": "東京"
-        },
-        "large_service_area": {
-          "code": "SS10",
-          "name": "関東"
-        },
-        "lat": 35.6582259169,
-        "lng": 139.3381858122,
-        "logo_image": "https://imgfp.hotp.jp/IMGH/61/98/P038366198/P038366198_69.jpg",
-        "lunch": "なし",
-        "middle_area": {
-          "code": "Y110",
-          "name": "八王子・立川"
-        },
-        "midnight": "営業している",
-        "mobile_access": "JR八王子駅北口徒歩1分/京王八王子駅徒歩3分",
-        "name": "隠れ家バル Funny&Bouquet",
-        "name_kana": "かくれやばる　ふぁにーあんどぶーけ",
-        "non_smoking": "禁煙席なし",
-        "open": "月～日、祝日、祝前日: 17:00～翌5:00 （料理L.O. 翌3:00 ドリンクL.O. 翌4:00）",
-        "other_memo": "個室や貸切の詳細等お気軽にお電話にてお問い合わせください。八王子での飲み会に◎",
-        "parking": "なし ：お近くのコインパーキングをご利用下さい。",
-        "party_capacity": 50,
-        "pet": "不可",
-        "photo": {
-          "mobile": {
-            "l": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_168.jpg",
-            "s": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_100.jpg"
-          },
-          "pc": {
-            "l": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_238.jpg",
-            "m": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_168.jpg",
-            "s": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_58_s.jpg"
-          }
-        },
-        "private_room": "なし ：個室　2名×4室　4名×8室　8名～10名×4室　20名～40名×2室",
-        "service_area": {
-          "code": "SA11",
-          "name": "東京"
-        },
-        "shop_detail_memo": "★NEW OPEN★当店イチ押しのコース★2.5h飲み放題付き「Funny Classicコース」4980円→2980円",
-        "show": "なし",
-        "small_area": {
-          "code": "X220",
-          "name": "八王子"
-        },
-        "station_name": "八王子",
-        "sub_genre": {
-          "code": "G002",
-          "name": "ダイニングバー・バル"
-        },
-        "tatami": "なし ：八王子で人気のお座敷個室！最大50名様まで対応できます。",
-        "tv": "あり",
-        "urls": {
-          "pc": "https://www.hotpepper.jp/strJ001275177/?vos=nhppalsa000016"
-        },
-        "wedding": "八王子での結婚式の二次会や同窓会、打ち上げパーティー等多様なシーンにご利用下さい",
-        "wifi": "あり"
-      }
-    ]
+  let oldlist = document.querySelector('ul#hot');
+  if (oldlist != undefined) {
+    oldlist.remove();
   }
-};
+  let keywords = [
+    "居酒屋",
+    "ダイニングバー・バル",
+    "創作料理",
+    "和食",
+    "洋食",
+    "イタリアン・フレンチ",
+    "中華",
+    "焼肉・ホルモン",
+    "アジア・エスニック料理",
+    "各国料理",
+    "カラオケ・パーティ",
+    "バー・カクテル",
+    "ラーメン",
+    "カフェ・スイーツ",
+    "その他グルメ",
+    "お好み焼き・もんじゃ",
+    "韓国料理"
+  ]
 
+  let ul = document.createElement('ul');
+  ul.setAttribute('id','hot');
+
+  for (let i = 0; i < keywords.length; i++) {
+      let li = document.createElement('li');
+      li.textContent = keywords[i];
+      ul.insertAdjacentElement('beforeend', li);
+  }
+  let osusumeBtn = document.querySelector('button#kensaku');
+  osusumeBtn.insertAdjacentElement('afterend', ul);
+} 
+let osusumeBtn = document.querySelector('button#osusume');
+osusumeBtn.addEventListener('click', suggest);
